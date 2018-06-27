613a22939485ee3d0bad22ba5dd4c545275c4503
Piter Ortiz <piter_og@hotmail.com>
2018-06-26 23:25:10 -0300

    Changins Contribuicao Model

--- app/Contribuicao.php
+++ app/Contribuicao.php
@@ -9,6 +9,6 @@ class Contribuicao extends Model
     protected $table = 'contribuicoes';
 
     protected $fillable = [
-        'nome', 'email', 'telefone', 'cidade', 'area', 'sugestao',
+        'nome', 'email', 'telefone', 'cidade', 'area', 'sugestao', 'visivel',
     ];
 }
